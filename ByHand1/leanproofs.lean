df4ee3005789711a2c50c72271840b93f9387ddf
wpaek <wpaek@hawaii.edu>
2026-07-24 20:50:29 -0500

    remove ByHand1 directory

--- ByHand1/leanproofs.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib
-
--- Z/2Z stuff
-theorem z2_abelian_add (x y : ZMod 2) : x + y = y + x := by
-  exact add_comm x y
-
--- Z/3Z stuff
-theorem z3_abelian_add (x y : ZMod 3) : x + y = y + x := by
-  exact add_comm x y
